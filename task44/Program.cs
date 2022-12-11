e8d8d06655c5f8896004c35d32d907e977fc4d15
Елена <egoncharova72@yandex.ru>
2022-12-12 01:13:36 +0400

    44

---
+++ task44/Program.cs
@@ -0,0 +1,36 @@
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+Console.Clear();
+ int[]Fibonachi(int n , int num1, int num2)
+ {
+  int[]arr= new int[n];
+  num1=0;
+  num2=1;
+
+  for (int i = 2; i < n; i++)
+  {
+    arr[0]=num1;
+    arr[1]= num2;
+    arr[i]= arr[i-1]+arr[i-2] ;
+
+
+  }
+   return arr;
+ }
+  
+  void PrintArr(int[]arr)
+{ 
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+      Console.Write($"{arr[i]},");
+    }
+    
+        Console.WriteLine("]");
+}
+
+int[]fibonachi=Fibonachi( 10,0,1);
+PrintArr( fibonachi);
---
+++ task45/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+ Console.Clear();
+
+//   int[] CopyArray(int[] massive)
+//  {
+//  int[] newArray = new int[massive.Length];
+//  for (int i = 0; i < massive.Length; i++)
+//  {
+//  newArray[i] = massive[i];
+//  }
+//  return newArray;
+//  }
+//  void PrintArray(int[] array)
+//  {
+//  for (int i = 0; i < array.Length; i++)
+//    {
+//    Console.Write(array[i] );
+//    }
+//  }
+//  int[] arr = { 5, 4, 6, 7 };
+// int[] newarr = CopyArray(arr);
+//  PrintArray(newarr); 
+
+
+
+  int[]CopyArray(int[]arr)
+  {
+     int[]newarr= new int[ arr.Length];
+     for (int i = 0; i <arr. Length; i++)   
+    {
+       newarr[i] = arr[i];
+    }
+
+      return newarr;
+ }
+
+ void PrintArr(int[]arr)
+ { 
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+     {        if (i<arr.Length-1) Console.Write($"{arr[i]}, ");
+       else Console.Write($"{arr[i]}");
+     }
+    
+        Console.WriteLine("]");
+}
+
+
+  int [] massive={1,2,3, 4,5};
+  PrintArr(massive);
+   massive[0]=55;//  демонстрация того что в скопированном массиве можно менять числа.  
+  int[] сopyArray=CopyArray (massive);
+  
+  PrintArr(сopyArray);
