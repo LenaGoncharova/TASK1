ca1160e6da56e9b84f3bd2e8bd20971feeb3b4d2
Елена <egoncharova72@yandex.ru>
2022-12-15 18:59:01 +0400

    task46

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,20 +1,7 @@
-﻿// таблица умножения
-using System;
-using static System.Console;
-Clear();
+﻿// перевод в другой метод счисления
 
-int[] CreateArr(int size, int min, int max)
-{
-    int[] arr = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rnd.Next(min, max + 1);
-        
-    }
-    return arr;
-}
- int[] сreateArr=CreateArr(5,4,12);
- 
-WriteLine(String.Join(",",сreateArr));
- 
+Console.Clear();
+Console.Write("введите число  ");
+int n = int.Parse(Console.ReadLine());
+ string s= Convert.ToString(n,2);
+ Console.WriteLine(s);
