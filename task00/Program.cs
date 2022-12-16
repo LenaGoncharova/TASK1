6fe32f3f3b71735b2226605050e53d7cc2a2d0db
Елена <egoncharova72@yandex.ru>
2022-12-16 23:18:40 +0400

    task46

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,7 +1,2 @@
-﻿// перевод в другой метод счисления
-
-Console.Clear();
-Console.Write("введите число  ");
-int n = int.Parse(Console.ReadLine());
- string s= Convert.ToString(n,2);
- Console.WriteLine(s);
+﻿Console.Clear();
+Console.Write("привет");
