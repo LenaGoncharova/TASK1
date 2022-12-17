d6c0594c928f92e05d51c00c4a57ff1f3f7ca3cb
Елена <egoncharova72@yandex.ru>
2022-12-17 16:44:36 +0400

    task46

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,9 @@
-﻿Console.Clear();
-Console.Write("привет");
+﻿// Считывание однромерного массива из консоли
+
+using System;
+using static System.Console;
+Clear();
+Write ( " ввдедите значения в консоль через пробел : ");
+string[] nums= ReadLine().Split( "  ", StringSplitOptions.TrimEntries);
+// int [] array= GetArray ( int. Parse( nums [0]), int. Parse ( nums  [1]), 0, 10);
+
