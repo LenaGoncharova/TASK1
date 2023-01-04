9a0e1b1cead0f189ed85998ec94fe042ce8b7d30
Елена <egoncharova72@yandex.ru>
2023-01-05 02:42:04 +0400

    task

---
+++ task63/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+Console.Clear();
+ Console.Write("Введите натуральное число N: ");
+ int n=Convert.ToInt32(Console.ReadLine());
+//  1 способ с использованием return 
+//  void NaturalNumber( int number)
+//  {
+//     if(number==0) return ;
+//     NaturalNumber( number-1);
+//     Console.Write($"{number}  ");
+//  }
+//  2 способ
+ void NaturalNumber( int number)
+ {
+    if(number>0)
+    { NaturalNumber( number-1);
+    Console.Write($"{number}  ");
+    }
+ }
+
+    
+ NaturalNumber(n);
+ Console.WriteLine();
+  NaturalNumber(n);
